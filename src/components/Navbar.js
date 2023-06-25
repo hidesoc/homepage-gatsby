b8b3db9b1dc3a7666e5e767a02010895b2ef6905
hidesoc <97414367+hidesoc@users.noreply.github.com>
2023-06-25 10:11:36 +0900

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,13 +33,13 @@ const Navbar = () => {
                 to a ul for accessibilty purposes, would like to see a css
                 re-write that makes this unneccesary.
              */}
-            /*
+            {/*
             <li className="navbar-item" style={{padding: "0px"}}>
               <Link className="navbar-item" to="/about">
                 About
               </Link>
             </li>
-            */
+             */}
             <li className="navbar-item" style={{padding: "0px"}}>
             <Link className="navbar-item" to="/products">
               Products
@@ -55,13 +55,13 @@ const Navbar = () => {
               Contact
             </Link>
             </li>
-            /*
+            {/*
             <li className="navbar-item" style={{padding: "0px"}}>
             <Link className="navbar-item" to="/contact/examples">
               Form Examples
             </Link>
             </li>
-            */
+             */}
           <li className="navbar-end has-text-centered">
             <a
               className="navbar-item"
